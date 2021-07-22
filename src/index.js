8509d53123f5ebf01882c4ec2553fdff7d47fec9
Willy Antunez <willy.e.antunez@gmail.com>
2021-07-22 08:34:02 -0600

    configuracion finalizada

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReacDom from 'react-dom';
+import App from './components/App';
+import './styles/global.scss';
+
+ReacDom.render(<App />, document.getElementById('app'));
